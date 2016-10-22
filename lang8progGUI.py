29bf1d2b9de04466db8841fd23a649a9e8bf7f2f
filipyoo <philippe_khin@hotmail.fr>
2016-10-22 22:35:30 +0900

    Update lang8progGUI.py

--- lang8progGUI.py
+++ lang8progGUI.py
@@ -17,22 +17,21 @@ class Lang8UIprogram(Ui_Dialog):
 
 		# When the GUI is launch, it displays the last entry read, by checking it ou from the lastReadEntryIndex.txt file
 		self.last_read_entry_index = int(open("lastReadEntryIndex.txt").read())
-		last_read_entry_text = open("yamasvEntries\\%d.txt" % self.last_read_entry_index, encoding="utf-8").read()
-		self.current_page = self.last_read_entry_index
+		last_read_entry_text       = open("yamasvEntries\\%d.txt" % self.last_read_entry_index, encoding="utf-8").read()
+		self.current_page          = self.last_read_entry_index
 		self.currentPageDisplay.setText(str(self.current_page)+"/"+str(entries_total))
 		self.textBrowser.setPlainText(last_read_entry_text)
 
-
 		# To make your GUI elements available to the rest of the object, preface them with self.
 		# random, previous and next page is precede by "self", ortherwise it can't be a "global" variable 
-		self.random_page_index = randint(1, entries_total)
-		random_page_text = open("yamasvEntries\\%d.txt" % self.random_page_index, encoding="utf-8").read()
-
-		self.next_page_index = self.current_page + 1 if self.current_page < entries_total else self.current_page
-		next_page_text = open("yamasvEntries\\%d.txt" % self.next_page_index, encoding="utf-8").read()
-
+		self.random_page_index   = randint(1, entries_total)
+		random_page_text         = open("yamasvEntries\\%d.txt" % self.random_page_index, encoding="utf-8").read()
+		
+		self.next_page_index     = self.current_page + 1 if self.current_page < entries_total else self.current_page
+		next_page_text           = open("yamasvEntries\\%d.txt" % self.next_page_index, encoding="utf-8").read()
+		
 		self.previous_page_index = self.current_page - 1 if self.current_page > 1 else self.current_page
-		previous_page_text = open("yamasvEntries\\%d.txt" % self.previous_page_index, encoding="utf-8").read()
+		previous_page_text       = open("yamasvEntries\\%d.txt" % self.previous_page_index, encoding="utf-8").read()
 
 		# Describe what function to call, when the specific button is clicked 
 		self.randomButton.clicked.connect(self.randomPageDisplay(random_page_text))
@@ -44,69 +43,44 @@ class Lang8UIprogram(Ui_Dialog):
 	def randomPageDisplay(self, *args):
 		def randomPage():
 			self.random_page_index = randint(1, entries_total)
-			random_page_text = open("yamasvEntries\\%d.txt" % self.random_page_index, encoding="utf-8").read()
-			self.textBrowser.setPlainText(random_page_text)
-			self.current_page = self.random_page_index
-			self.currentPageDisplay.setText(str(self.current_page)+"/"+str(entries_total))
-			f = open("lastReadEntryIndex.txt", "w")
-			f.write(str(self.current_page))
-			f.close()
-			# updateScreenWith(self, random_page_index)
+			self.updateScreenWith(self.random_page_index)
 		return randomPage
 
 	def nextPageDisplay(self, *args):
 		def nextPage():
 			self.next_page_index = self.current_page + 1 if self.current_page < entries_total else self.current_page
-			next_page_text = open("yamasvEntries\\%d.txt" % self.next_page_index, encoding="utf-8").read()
-			self.textBrowser.setPlainText(next_page_text)
-			self.current_page = self.next_page_index
-			self.currentPageDisplay.setText(str(self.current_page)+"/"+str(entries_total))
-			f = open("lastReadEntryIndex.txt", "w")
-			f.write(str(self.current_page))
-			f.close()
-			# updateScreenWith(self, next_page_index)
+			self.updateScreenWith(self.next_page_index)
 		return nextPage
 
 	def previousPageDisplay(self, *args):
 		def previousPage():
 			self.previous_page_index = self.current_page - 1 if self.current_page > 1 else self.current_page
-			previous_page_text = open("yamasvEntries\\%d.txt" % self.previous_page_index, encoding="utf-8").read()
-			self.textBrowser.setPlainText(previous_page_text)
-			self.current_page = self.previous_page_index
-			self.currentPageDisplay.setText(str(self.current_page)+"/"+str(entries_total))
-			f = open("lastReadEntryIndex.txt", "w")
-			f.write(str(self.current_page))
-			f.close()
-			# updateScreenWith(self, previous_page_index)
+			self.updateScreenWith(self.previous_page_index)
 		return previousPage
 
 
-	# def updateScreenWith (self, page_to_display_index):
-		# page_to_display_text = open("yamasvEntries\\%d.txt" % self.page_to_display_index, encoding="utf-8").read()
-		# self.textBrowser.setPlainText(page_to_display_text)
-		# self.current_page = self.page_to_display_index
-		# self.currentPageDisplay.setText(str(self.current_page)+"/"+str(entries_total))
+	def updateScreenWith (self, page_to_display_index):
+		page_to_display_text = open("yamasvEntries\\%d.txt" % page_to_display_index, encoding="utf-8").read()
+		self.textBrowser.setPlainText(page_to_display_text)
+		self.current_page = page_to_display_index
+		self.currentPageDisplay.setText(str(self.current_page)+"/"+str(entries_total))
+		f = open("lastReadEntryIndex.txt", "w")
+		f.write(str(self.current_page))
+		f.close()
 
 	def searchPage(self, *args):
-		if self.pageSearchLine.text().isnumeric() and int(self.pageSearchLine.text()) in range (1,entries_total+1):
-			page_to_search = int(self.pageSearchLine.text())  
-			searched_page_text = open("yamasvEntries\\%d.txt" % page_to_search, encoding="utf-8").read()
-			self.textBrowser.setPlainText(searched_page_text)
-			self.current_page = page_to_search
-			self.currentPageDisplay.setText(str(self.current_page)+"/"+str(entries_total))
-			f = open("lastReadEntryIndex.txt", "w")
-			f.write(str(self.current_page))
-			f.close()
+		if self.pageSearchLine.text().isnumeric() and int(self.pageSearchLine.text()) in range (1, entries_total+1):
+			page_to_search_index = int(self.pageSearchLine.text())  
+			self.updateScreenWith(page_to_search_index)
 		
-
 
 
 if __name__ == '__main__':
 
 	entries_total = len(os.listdir("yamasvEntries"))
-
-	app = QtWidgets.QApplication(sys.argv)
-	dialog = QtWidgets.QDialog() 
-	prog = Lang8UIprogram(dialog)
+	
+	app           = QtWidgets.QApplication(sys.argv)
+	dialog        = QtWidgets.QDialog() 
+	prog          = Lang8UIprogram(dialog)
 	dialog.show()
 	sys.exit(app.exec_())
